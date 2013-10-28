1880dcc53194ae7d091fc515ff2f22d47c3745c3
obfuscurity <jason@dixongroup.net>
2013-10-28 16:01:00 -0400

    site and event update functionality

--- lib/radial/models/sites.rb
+++ lib/radial/models/sites.rb
@@ -3,5 +3,16 @@ class Site
   many :events
 
   key :name, String
+  key :url, String
+
   timestamps!
+
+  def self.update_all
+    Site.all.each do |site|
+      nagios = NagiosHarder::Site.new("#{site.url}/cgi-bin/", ENV['NAGIOS_USER'], ENV['NAGIOS_PASS'])
+      t1 = Time.now.to_i
+      alerts = nagios.service_status(:critical).count
+      puts "site #{site} took #{time.now.to_i - t1} sec for #{alerts} alerts"
+    end
+  end
 end
